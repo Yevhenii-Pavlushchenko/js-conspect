d11a4f2847c897ab68ab5850b893d638072d0045
JustFun <paw93@ukr.net>
2026-01-07 16:13:09 +0200

    ##2

--- js/js-module-1.js
+++ js/js-module-1.js
@@ -1,12 +1,24 @@
-// console.log(window);
-const userAnswer = prompt("do you agree")
-console.log("🚀 ~ result:", userAnswer)
-console.log("🚀 ~ result:", typeof userAnswer)
+// // console.log(window);
+// const userAnswer = prompt("do you agree");
+// console.log("🚀 ~ result:", userAnswer);
+// console.log("🚀 ~ result:", typeof userAnswer);
 
+// if (typeof userAnswer === "string") {
+//   const age = Number(userAnswer);
+//   //   console.log(age);
+//   //   console.log(typeof age);
+// }
+// const a = 15;
+// const b = 10;
+// console.log(a - b);
 
+const abc = "rome odesa stambul";
 
-if (typeof userAnswer === 'string') {
-    const age = Number(userAnswer);
-    console.log(age);
-    console.log(typeof age);
-}
+const abcLength = abc.length;
+console.log(abcLength);
+
+const words = abc.split(" ");
+console.log(words);
+
+const res = "" * 8 + 3;
+console.log(res);
