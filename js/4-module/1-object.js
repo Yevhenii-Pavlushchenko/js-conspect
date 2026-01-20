793ce84f7c3cd6c79cfaea7e33f9d0a05f684d2f
JustFun <paw93@ukr.net>
2026-01-20 14:10:46 +0200

    +++

--- js/4-module/1-object.js
+++ js/4-module/1-object.js
@@ -4,9 +4,43 @@ const apartment = {
   rating: 4,
   price: 2153,
   tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
 };
+// console.log("🚀 ~ apartment.owner:", apartment.owner);
+const aptImgUrl = apartment["imgUrl"];
+const descr = apartment["descr"];
+const aprRating = apartment["rating"];
+const aprPrice = apartment["price"];
+let aprTags = apartment["tags"];
+let aprOwner = apartment["owner"];
+// console.log("🚀 ~ aprTags:", aprTags);
+// console.log("🚀 ~ aptImgUrl:", aptImgUrl);
+// console.log("🚀 ~ descr:", descr);
+// console.log("🚀 ~ aprRating:", aprRating);
+// console.log("🚀 ~ aprPrice:", aprPrice);
+// console.log("🚀 ~ aprTags:", aprTags);
+// console.log("🚀 ~ aprOwner:", aprOwner);
 // console.log("🚀 ~ apartment:", apartment.descr);
 
+console.log("🚀 ~ apartment.tags BEFORE:", apartment.tags);
+console.log("🚀 ~ aprTags BEFORE:", aprTags);
+// apartment.tags = ["premium", "promoted", "top", "free"];
+apartment.tags.push("free");
+console.log("🚀 ~ apartment.tags AFTER:", apartment.tags);
+console.log("🚀 ~ aprTags AFTER:", aprTags);
+
+// console.log("🚀 ~ apartme÷nt.owner BEFORE:", apartment.owner);
+// console.log("🚀 ~ aprOwn÷er BEFORE:", aprOwner);
+apartment.owner.phone = "063-000-00-00";
+// apartment.owner.phone2 = "063-777-77-77";
+
+// console.log("🚀 ~ apartment.owner AFTER:", apartment.owner);
+// console.log("🚀 ~ aprOwner AFTER:", aprOwner);
+//
 const user = {
   name: "Jacques Gluke",
   tag: "jgluke",
@@ -20,5 +54,25 @@ const user = {
     likes: 1308,
   },
 };
-console.log("🚀 ~ user.stats.likes:", user.stats.likes);
-console.log(user.stats.views);
+// console.log("🚀 ~ user.stats.likes:", user.stats.likes);
+// console.log(user.stats.views);
+
+const name = "Henry Sibola";
+const age = 25;
+
+const user2 = {
+  name,
+  age,
+};
+
+// console.log(user2); // "Henry Sibola"
+// console.log(user2); // 25
+
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  [emailInputName]: "henry.carter@aptmail.com",
+  [passwordInputName]: "jqueryismyjam",
+};
+// console.log("🚀 ~ credentials:", credentials);
